c8bdc769f310ae8af3c705d45506b8f92b2fd655
Yan Dekov <StewieSolden147@gmail.com>
2023-11-24 08:16:21 +0500

    init migrations

---
+++ webapi/Migrations/20231124031331_init.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Net;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "allowedmime",
+                columns: table => new
+                {
+                    mime_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    mime_name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_allowedmime", x => x.mime_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    username = table.Column<string>(type: "text", nullable: false),
+                    role = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "api",
+                columns: table => new
+                {
+                    api_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    api_key = table.Column<string>(type: "text", nullable: false),
+                    remote_ip = table.Column<IPAddress>(type: "inet", nullable: true),
+                    is_tracking_ip = table.Column<bool>(type: "boolean", nullable: false),
+                    is_allowed_requesting = table.Column<bool>(type: "boolean", nullable: false),
+                    is_allowed_unknown_ip = table.Column<bool>(type: "boolean", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_api", x => x.api_id);
+                    table.ForeignKey(
+                        name: "FK_api_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "files",
+                columns: table => new
+                {
+                    file_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    file_name = table.Column<string>(type: "text", nullable: false),
+                    file_mime = table.Column<string>(type: "text", nullable: false),
+                    type = table.Column<string>(type: "text", nullable: true),
+                    operation_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_files", x => x.file_id);
+                    table.ForeignKey(
+                        name: "FK_files_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "keys",
+                columns: table => new
+                {
+                    key_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    private_key = table.Column<string>(type: "text", nullable: false),
+                    person_internal_key = table.Column<string>(type: "text", nullable: true),
+                    received_internal_key = table.Column<string>(type: "text", nullable: true),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_keys", x => x.key_id);
+                    table.ForeignKey(
+                        name: "FK_keys_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "notifications",
+                columns: table => new
+                {
+                    notification_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    message_header = table.Column<string>(type: "text", nullable: false),
+                    message = table.Column<string>(type: "text", nullable: false),
+                    priority = table.Column<string>(type: "text", nullable: false),
+                    send_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_checked = table.Column<bool>(type: "boolean", nullable: false),
+                    sender_id = table.Column<int>(type: "integer", nullable: false),
+                    receiver_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_notifications", x => x.notification_id);
+                    table.ForeignKey(
+                        name: "FK_notifications_users_receiver_id",
+                        column: x => x.receiver_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_notifications_users_sender_id",
+                        column: x => x.sender_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "offers",
+                columns: table => new
+                {
+                    offer_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    offer_header = table.Column<string>(type: "text", nullable: false),
+                    offer_body = table.Column<string>(type: "text", nullable: false),
+                    offer_type = table.Column<string>(type: "text", nullable: false),
+                    is_accepted = table.Column<bool>(type: "boolean", nullable: false),
+                    sender_id = table.Column<int>(type: "integer", nullable: false),
+                    receiver_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_offers", x => x.offer_id);
+                    table.ForeignKey(
+                        name: "FK_offers_users_receiver_id",
+                        column: x => x.receiver_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_offers_users_sender_id",
+                        column: x => x.sender_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tokens",
+                columns: table => new
+                {
+                    token_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    refresh_token = table.Column<string>(type: "text", nullable: true),
+                    expiry_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tokens", x => x.token_id);
+                    table.ForeignKey(
+                        name: "FK_tokens_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_api_user_id",
+                table: "api",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_files_user_id",
+                table: "files",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_keys_user_id",
+                table: "keys",
+                column: "user_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_notifications_receiver_id",
+                table: "notifications",
+                column: "receiver_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_notifications_sender_id",
+                table: "notifications",
+                column: "sender_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_offers_receiver_id",
+                table: "offers",
+                column: "receiver_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_offers_sender_id",
+                table: "offers",
+                column: "sender_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tokens_user_id",
+                table: "tokens",
+                column: "user_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "allowedmime");
+
+            migrationBuilder.DropTable(
+                name: "api");
+
+            migrationBuilder.DropTable(
+                name: "files");
+
+            migrationBuilder.DropTable(
+                name: "keys");
+
+            migrationBuilder.DropTable(
+                name: "notifications");
+
+            migrationBuilder.DropTable(
+                name: "offers");
+
+            migrationBuilder.DropTable(
+                name: "tokens");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
