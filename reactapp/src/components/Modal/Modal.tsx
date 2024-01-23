39b0751f12bd10f54c81aa2e5f47e30f3df256c3
Yan Dekov <StewieSolden147@gmail.com>
2024-01-23 10:14:17 +0500

    Added modal window

--- reactapp/src/components/Modal/Modal.tsx
+++ reactapp/src/components/Modal/Modal.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Modal.css'
 
 function Modal({ isActive, setActive, children }: ModalProps) {
     return (
-        <div className={isActive ? "modal-active" : "modal"}>
-            <div className={isActive ? "modal-content-active" : "modal-content"}>
+        <div className={isActive ? "modal active" : "modal"} onClick={() => setActive(false)}>
+            <div className={isActive ? "modal-content active" : "modal-content"} onClick={e => e.stopPropagation()}>
                 {children}
             </div>
         </div>
