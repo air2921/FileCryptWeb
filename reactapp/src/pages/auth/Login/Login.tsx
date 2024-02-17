940af174a8f5a04d2ea30aa1ae60a5f950e08476
Yan Dekov <StewieSolden147@gmail.com>
2024-02-17 05:49:30 +0500

    login page styled

--- reactapp/src/pages/auth/Login/Login.tsx
+++ reactapp/src/pages/auth/Login/Login.tsx
@@ -4,6 +4,7 @@ import AxiosRequest from '../../../utils/api/AxiosRequest';
 import Modal from '../../../components/Modal/Modal';
 import CreateRecovery from '../recovery/CreateRecovery';
 import Message from '../../../utils/helpers/message/Message';
+import './Login.css'
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -49,38 +50,50 @@ const Login = () => {
             }
             else {
                 setVerificationErrorMessage(response.data);
+
+                setTimeout(() => {
+                    setVerificationErrorMessage('');
+                }, 5000)
             }
         };
 
         return (
-            <div>
-                <p>Two-Factor Authentication</p>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="code">
-                        Enter your numeric code from your email
-                        <input
-                            type="text"
-                            id="code"
-                            required={true}
-                            value={code}
-                            onChange={(e) => {
-                                const value = e.target.value;
-                                if (value === '') {
-                                    setCode(undefined);
-                                } else {
-                                    const parsedValue = parseInt(value, 10);
-                                    if (!isNaN(parsedValue)) {
-                                        setCode(parsedValue);
+            <div className="verify-container">
+                <div className="verify-header">
+                    Two-Factor Authentication
+                </div>
+                <form className="verify-form" onSubmit={handleSubmit}>
+                    <div className="code-text">Enter your numeric code from your email</div>
+                    <div className="code-container">
+                        <label htmlFor="code" className="code-label">
+                            <input className="code-input"
+                                type="text"
+                                id="code"
+                                required={true}
+                                value={code}
+                                onChange={(e) => {
+                                    const value = e.target.value;
+                                    if (value === '') {
+                                        setCode(undefined);
+                                    } else {
+                                        const parsedValue = parseInt(value, 10);
+                                        if (!isNaN(parsedValue)) {
+                                            setCode(parsedValue);
+                                        }
                                     }
-                                }
-                            }}
-                            inputMode="numeric"
-                            placeholder="Code"
-                        />
-                    </label>
-                    <button type="submit">Verify</button>
+                                }}
+                                inputMode="numeric"
+                                placeholder="Code"
+                            />
+                        </label>
+                    </div>
+                    <div className="verify-btn-container">
+                        <button className="verify-btn" type="submit">Verify</button>
+                    </div>
                 </form>
-                {errorVerificationMessage && <Message message={errorVerificationMessage} font='error' />}
+                <div className="verify-message">
+                    {errorVerificationMessage && <Message message={errorVerificationMessage} font='error' />}
+                </div>
             </div>
         );
     }
@@ -88,36 +101,46 @@ const Login = () => {
     return (
         <div className="login">
             <div className="login-container">
-                <p className="welcome-text">Sign in to FileCryptWeb</p>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">
-                        {email ? "Email" : "* Email"}
-                        <input
-                            type="email"
-                            id="email"
-                            required={true}
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </label>
-                    <label htmlFor="password">
-                        {password ? "Password" : "* Password"}
-                        <input
-                            type="password"
-                            id="password"
-                            required={true}
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </label>
-                    <div className="login-signup-btn-container">
-                        <button type="submit">Login</button>
-                        <button onClick={() => navigate('/auth/signup')}>Register</button>
+                <div className="login-header">
+                    Sign in to FileCryptWeb
+                </div>
+                <form className="login-form" onSubmit={handleSubmit}>
+                    <div className="input-container">
+                        <div className="email-container">
+                            <div className="email-text">Email</div>
+                            <label htmlFor="email" className="email-label">
+                                <input className="email-input"
+                                    type="email"
+                                    id="email"
+                                    required={true}
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="Email"
+                                />
+                            </label>
+                        </div>
+                        <div className="password-container">
+                            <div className="password-text">Password</div>
+                            <label htmlFor="password" className="password-label">
+                                <input className="password-input"
+                                    type="password"
+                                    id="password"
+                                    required={true}
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="Password"
+                                />
+                            </label>
+                        </div>
                     </div>
-                    <div className="recovery-btn-container">
-                        <button onClick={() => setRecovery(true)}>Recovery account</button>
+                    <div className="auth-container">
+                        <button className="signin-form-btn" type="submit">Login</button>
+                        <button className="signup-form-btn" onClick={() => navigate('/auth/signup')}>Register</button>
                     </div>
                 </form>
+                <div className="recovery-btn-container">
+                    <button className="recovery-btn" onClick={() => setRecovery(true)}>Recovery account</button>
+                </div>
             </div>
             <Modal isActive={verificationRequired} setActive={setVerification}>
                 <Verify />
@@ -125,7 +148,9 @@ const Login = () => {
             <Modal isActive={recoveryAccount} setActive={setRecovery}>
                 <CreateRecovery />
             </Modal>
-            {errorMessage && <Message message={errorMessage} font='error' />}
+            <div className="message">
+                {errorMessage && <Message message={errorMessage} font='error' />}
+            </div>
         </div>
     );
 };
