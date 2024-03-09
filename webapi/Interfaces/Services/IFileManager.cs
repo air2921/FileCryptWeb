20a03ef61967d5c7c2b03978124082859ab5a341
Yan Dekov <StewieSolden147@gmail.com>
2024-03-10 01:51:56 +0500

    new tests

--- webapi/Interfaces/Services/IFileManager.cs
+++ webapi/Interfaces/Services/IFileManager.cs
@@ -1,9 +1,12 @@
-﻿namespace webapi.Interfaces.Services
+﻿using webapi.Models;
+
+namespace webapi.Interfaces.Services
 {
     public interface IFileManager
     {
-        public string[] GetCsvFiles();
         public HashSet<string> GetMimesFromCsvFile(string filePath);
         public string GetReactAppUrl(string path, bool isChrome);
+        public void AddSecureCollection(ref HashSet<FileMimeModel> mimeModels, HashSet<string> existingMimes);
+        public void AddFullCollection(ref HashSet<FileMimeModel> mimeModels, HashSet<string> existingMimes);
     }
 }
