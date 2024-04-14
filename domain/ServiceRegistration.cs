0494a846ec35ef208a3492cfed18cef015e7eba6
Yan Dekov <StewieSolden147@gmail.com>
2024-04-14 22:10:25 +0700

    wip

--- domain/ServiceRegistration.cs
+++ domain/ServiceRegistration.cs
@@ -5,6 +5,7 @@ using domain.Models;
 using domain.Services;
 using domain.Services.Abstractions;
 using domain.Services.Additional.Account;
+using domain.Services.Additional.Account.Edit;
 using domain.Services.Master_Services.Account;
 using domain.Services.Master_Services.Account.Edit;
 using domain.Upper_Module.Services;
@@ -25,18 +26,23 @@ namespace domain
             services.AddScoped<Secret>();
             services.AddScoped<IValidation, Validation>();
 
-            services.AddScoped<IRegistrationService, RegistrationService>();
-            services.AddScoped<ISessionService, SessionService>();
-            services.AddScoped<IRecoveryService, RecoveryService>();
-            services.AddScoped<IUsernameService, UsernameService>();
-            services.AddScoped<IPasswordService, PasswordService>();
-            services.AddScoped<I2FaService, _2FaService>();
+            services.AddUpperModuleServices();
+            services.AddCacheServices();
+            services.AddKeyedServices();
+        }
 
-            services.AddScoped<ICacheHandler<FileModel>, Files>();
-            services.AddScoped<ICacheHandler<KeyModel>, Keys>();
-            services.AddScoped<ICacheHandler<NotificationModel>, Notifications>();
-            services.AddScoped<ICacheHandler<OfferModel>, Offers>();
-            services.AddScoped<ICacheHandler<UserModel>, Users>();
+        private static void AddKeyedServices(this IServiceCollection services)
+        {
+            services.AddKeyedScoped<ITransaction<UserModel>, _2FaHelper>(ImplementationKey.ACCOUNT_2FA_SERVICE);
+            services.AddKeyedScoped<IDataManagement, _2FaHelper>(ImplementationKey.ACCOUNT_2FA_SERVICE);
+            services.AddKeyedScoped<IValidator, _2FaHelper>(ImplementationKey.ACCOUNT_2FA_SERVICE);
+
+            services.AddKeyedScoped<ITransaction<UserModel>, EmailHelper>(ImplementationKey.ACCOUNT_EMAIL_SERVICE);
+            services.AddKeyedScoped<IDataManagement, EmailHelper>(ImplementationKey.ACCOUNT_EMAIL_SERVICE);
+            services.AddKeyedScoped<IValidator, EmailHelper>(ImplementationKey.ACCOUNT_EMAIL_SERVICE);
+
+            services.AddKeyedScoped<ITransaction<UserModel>, PasswordHelper>(ImplementationKey.ACCOUNT_PASSWORD_SERVICE);
+            services.AddKeyedScoped<IDataManagement, PasswordHelper>(ImplementationKey.ACCOUNT_PASSWORD_SERVICE);
 
             services.AddKeyedScoped<ITransaction<UserDTO>, RegistrationHelper>(ImplementationKey.ACCOUNT_REGISTRATION_SERVICE);
             services.AddKeyedScoped<IDataManagement, RegistrationHelper>(ImplementationKey.ACCOUNT_REGISTRATION_SERVICE);
@@ -44,6 +50,26 @@ namespace domain
 
             services.AddKeyedScoped<IDataManagement, SessionHelper>(ImplementationKey.ACCOUNT_SESSION_SERVICE);
             services.AddKeyedScoped<IValidator, RecoveryHelper>(ImplementationKey.ACCOUNT_RECOVERY_SERVICE);
+        }
+
+        private static void AddCacheServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICacheHandler<FileModel>, Files>();
+            services.AddScoped<ICacheHandler<KeyModel>, Keys>();
+            services.AddScoped<ICacheHandler<NotificationModel>, Notifications>();
+            services.AddScoped<ICacheHandler<OfferModel>, Offers>();
+            services.AddScoped<ICacheHandler<UserModel>, Users>();
+        }
+
+        private static void AddUpperModuleServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRegistrationService, RegistrationService>();
+            services.AddScoped<ISessionService, SessionService>();
+            services.AddScoped<IRecoveryService, RecoveryService>();
+            services.AddScoped<IUsernameService, UsernameService>();
+            services.AddScoped<IPasswordService, PasswordService>();
+            services.AddScoped<I2FaService, _2FaService>();
+            services.AddScoped<IEmailService, EmailService>();
         }
     }
 }
