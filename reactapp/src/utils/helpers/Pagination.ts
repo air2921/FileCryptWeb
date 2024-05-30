b9a8a4e3b49fffbd08a1195b704827cc6b17a407
Yan Dekov <StewieSolden147@gmail.com>
2024-05-30 17:23:26 +0700

    deleted frontend

--- reactapp/src/utils/helpers/Pagination.ts
+++
@@ -1,9 +0,0 @@
-export function handleLoadMore(count: number, currentSkip: number, onSkipChange: (count: number) => void) {
-    const newSkip = currentSkip + count;
-    onSkipChange(newSkip);
-}
-
-export function handleLoadLess(count: number, currentSkip: number, onSkipChange: (newSkip: number) => void) {
-    const newSkip = Math.max(0, currentSkip - count);
-    onSkipChange(newSkip);
-}
